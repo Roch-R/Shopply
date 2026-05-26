df4740d770ff783787a12ec444c2fc3be8bcfb3f
yaho1 <robertpahugot123456@gmail.com>
2026-05-26 11:04:56 +0800

    debug: add database debug endpoint and check default connection

--- myproduct-backend/config/database.php
+++ myproduct-backend/config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', env('MYSQL_URL') ? 'mysql' : 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
