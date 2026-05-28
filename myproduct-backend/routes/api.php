2bee9303d3d53408bd8cb2abea8edddf77894ad2
yaho1 <robertpahugot123456@gmail.com>
2026-05-28 19:57:38 +0800

    diagnose: Add php.ini checks to debug endpoint

--- myproduct-backend/routes/api.php
+++ myproduct-backend/routes/api.php
@@ -79,5 +79,12 @@ Route::get('/debug-logs', function() {
         'nginx_error' => $tail($nginxError),
         'nginx_access' => $tail($nginxAccess),
         'laravel_log' => $tail($laravelLog),
+        'php_ini' => [
+            'upload_max_filesize' => ini_get('upload_max_filesize'),
+            'post_max_size' => ini_get('post_max_size'),
+            'memory_limit' => ini_get('memory_limit'),
+            'max_execution_time' => ini_get('max_execution_time'),
+            'max_input_time' => ini_get('max_input_time'),
+        ]
     ]);
 });
